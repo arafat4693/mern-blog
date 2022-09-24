d906d1a470a145db8bee8ff34286dd2b02e77113
Sunny <islamarafat171@gmail.com>
2022-09-24 10:58:53 +0200

    changed the axios base url.

--- client/src/utils/axiosConfig.ts
+++ client/src/utils/axiosConfig.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://mern-blog-server.onrender.com",
   withCredentials: true,
 })
 
